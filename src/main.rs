017bca2c99ddb65a246b3a3635d504949aa81f31
Stefan Sundin <git@stefansundin.com>
2023-04-09 16:07:01 -0700

    Add --wait to wait for the update to propagate before exiting.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // Copyright 2023 Stefan Sundin
 // Licensed under GNU GPL v3 or later
 
-use aws_sdk_route53::types::RrType;
+use aws_sdk_route53::types::{ChangeStatus, RrType};
 use clap::Parser;
 use std::str::FromStr;
+use std::{thread, time};
 
 #[derive(Parser)]
 #[command(arg_required_else_help(true))]
@@ -19,6 +20,9 @@ struct Arguments {
 
   #[arg(long, help = "DNS record value")]
   dns_value: String,
+
+  #[arg(long, help = "Wait for the change to propagate in Route 53")]
+  wait: bool,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -61,6 +65,25 @@ async fn main() -> Result<(), std::io::Error> {
     .expect("could not update DNS");
 
   println!("{:?}", response);
+
+  if args.wait {
+    let change_id = response.change_info().unwrap().id().unwrap();
+
+    loop {
+      thread::sleep(time::Duration::from_millis(1000));
+      let response = route53_client
+        .get_change()
+        .id(change_id)
+        .send()
+        .await
+        .expect("could not poll change status");
+      eprintln!("{:?}", response);
+      let change_status = response.change_info().unwrap().status().unwrap();
+      if matches!(change_status, ChangeStatus::Insync) {
+        break;
+      }
+    }
+  }
 
   return Ok(());
 }
